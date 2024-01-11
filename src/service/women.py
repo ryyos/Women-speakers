ca9221e475f7b5e3e0cd086c99a586f4d3916bd5
ryosyntaxx <riodwi12174@gmail.com>
2024-01-11 12:50:44 +0700

    Women v.0.0.1 Released

--- src/service/women.py
+++ src/service/women.py
@@ -2,11 +2,13 @@ import requests
 
 from time import time
 from datetime import datetime
-from icecream import ic
 from fake_useragent import FakeUserAgent
+from pyquery import PyQuery
+from urllib import request
 
 from src.utils.fileIO import File
 from src.utils.corrector import vname
+from src.utils.logs import logger
 
 
 class Women:
@@ -19,40 +21,45 @@ class Women:
         self.MAIN_URL = 'https://data.ipu.org/women-speakers'
         self.PIE_RIGHT_API = 'https://data.ipu.org/api/comparison.json?load-entity-refs=taxonomy_term%2Cfield_collection_item&max-depth=2&langcode=en&field=chamber%3Afield_chamber_speakers%3Afield_sex&structure=any__upper_chamber&chart_only=0'
         self.PIE_LEFT_API = 'https://data.ipu.org/api/comparison.json?load-entity-refs=taxonomy_term%2Cfield_collection_item&max-depth=2&langcode=en&field=chamber%3Afield_chamber_speakers%3Afield_sex&structure=any__lower_chamber&chart_only=0'
+        self.XLSX = 'https://data.ipu.org/sites/default/files/other-datasets/women_in_parliament-historical_database-1945_to_2018.xlsx'
+        self.RAW_PATH = 'data/xlsx/women_in_parliament-historical_database-1945_to_2018.xlsx'
 
-
+        self.GENDER = ['male', 'female']
 
         self.__header = {
             "User-Agent": self.__faker.random
         }
         ...
 
+    def __curl(self, path: str, url: str):
+        if url:
+            opener = request.build_opener()
+            opener.addheaders = [("User-Agent", self.__faker.random)]
+
+            request.install_opener(opener)
+            request.urlretrieve(url, path)
 
 
     def main(self):
         response = requests.get(self.PIE_LEFT_API+'&year='+'1993', headers=self.__header)
+        self.__curl(path=self.RAW_PATH, url=self.XLSX)
 
-        ic(response)
-        years = [year for year in response.json()["payload"]["visualization"]["map"]["settings"]["range"]]
+        logger.info('is downloading...')
+        logger.info('please wait..')
 
+        logger.info(f'status: {response.status_code}')
+        print()
+
+        years = [year for year in reversed(response.json()["payload"]["visualization"]["map"]["settings"]["range"])]
         for year in years:
-            ic(year)
 
-            ic(self.PIE_LEFT_API+'&year='+year)
-
-
+            logger.info(f'year: {year}')
             if year != '2024': 
                 left = requests.get(self.PIE_LEFT_API+'&year='+year, headers=self.__header)
                 right = requests.get(self.PIE_RIGHT_API+'&year='+year, headers=self.__header)
             else: 
                 left = requests.get(self.PIE_LEFT_API, headers=self.__header)
                 right = requests.get(self.PIE_RIGHT_API, headers=self.__header)
-
-            ic(left)
-            ic(right)
-
-            self.__file.write_json('data/left.json', left.json())
-            self.__file.write_json('data/right.json', right.json())
             
             results = {
                 "year": year,
@@ -60,10 +67,10 @@ class Women:
                 "url": self.MAIN_URL,
                 "crawler_time": str(datetime.now()),
                 "crawler_time_epoch": int(time()),
-                "path_data_row": "",
+                "path_data_row": self.RAW_PATH,
                 "chart": {
                     "lower_and_unicameral_chambers": {
-                        "total_speakers": "",
+                        "total_speakers": 0,
                         "female": {
                             "speakers": 0,
                             "percent": 0
@@ -74,7 +81,7 @@ class Women:
                         }
                     },
                     "upper_chambers": {
-                        "total_speakers": "",
+                        "total_speakers": 0,
                         "female": {
                             "speakers": 0,
                             "percent": 0
@@ -87,37 +94,26 @@ class Women:
                 }
             }
 
-            left_speakers = left.json()["payload"]["aggregations"]["chamber:field_chamber_speakers:field_sex"]["map"]
-            ic(len(left_speakers))
-            
-            right_speakers = right.json()["payload"]["aggregations"]["chamber:field_chamber_speakers:field_sex"]["map"]
-            ic(len(right_speakers))
+            left_speakers = PyQuery(left.json().get("payload", {}).get("visualization", {}).get("pie", {}).get("table", {}).get("markup", {})).find('tbody tr')
 
-            for speakers in left_speakers:
-                if speakers["value"] == "Male":
-                    results["chart"]["lower_and_unicameral_chambers"]["male"]["speakers"] += 1
-                else:
-                    results["chart"]["lower_and_unicameral_chambers"]["female"]["speakers"] += 1
+            speakers = [int(speaker) for speaker in left_speakers.find('td:last-child').text().split(' ')]
+            for index, speaker in enumerate(left_speakers):
 
-            results["chart"]["lower_and_unicameral_chambers"]["male"]["percent"] = \
-                str((results["chart"]["lower_and_unicameral_chambers"]["male"]["speakers"] / len(left_speakers) * 100))+'%'
-            results["chart"]["lower_and_unicameral_chambers"]["female"]["percent"] = \
-                str((results["chart"]["lower_and_unicameral_chambers"]["female"]["speakers"] / len(left_speakers) * 100))+'%'
-            
-            for speakers in right_speakers:
-                if speakers["value"] == "Male":
-                    results["chart"]["upper_chambers"]["male"]["speakers"] += 1
-                else:
-                    results["chart"]["upper_chambers"]["female"]["speakers"] += 1
+                results["chart"]["lower_and_unicameral_chambers"]["total_speakers"] = sum(speakers)
+                results["chart"]["lower_and_unicameral_chambers"][PyQuery(speaker).find('td:first-child').text().lower()]["speakers"] = speakers[index]
+                results["chart"]["lower_and_unicameral_chambers"][PyQuery(speaker).find('td:first-child').text().lower()]["percent"] = format((speakers[index] / sum(speakers) * 100), ".1f")+'%'
 
-            results["chart"]["upper_chambers"]["male"]["percent"] = \
-                str((results["chart"]["upper_chambers"]["male"]["speakers"] / len(left_speakers) * 100))+'%'
-            results["chart"]["upper_chambers"]["female"]["percent"] = \
-                str((results["chart"]["upper_chambers"]["female"]["speakers"] / len(left_speakers) * 100))+'%'
 
-            ic(results)
+            if right.json()["list"]: 
 
-            break
+                right_speakers = PyQuery(right.json().get("payload", {}).get("visualization", {}).get("pie", {}).get("table", {}).get("markup", {})).find('tbody tr')
+                speakers = [int(speaker) for speaker in right_speakers.find('td:last-child').text().split(' ')]
+                for index, speaker in enumerate(right_speakers):
 
+                    results["chart"]["upper_chambers"]["total_speakers"] = sum(speakers)
+                    results["chart"]["upper_chambers"][PyQuery(speaker).find('td:first-child').text().lower()]["speakers"] = speakers[index]
+                    results["chart"]["upper_chambers"][PyQuery(speaker).find('td:first-child').text().lower()]["percent"] = format((speakers[index] / sum(speakers) * 100), ".1f")+'%'
+
+            self.__file.write_json(path=f'data/json/{year}.json', content=results)
 
         ...
